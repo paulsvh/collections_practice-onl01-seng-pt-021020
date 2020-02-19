3d9c90ae53493213527e2fd8f59d2667bd6cb31a
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-19 05:36:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,13 +29,5 @@ def swap_elements_from_to(array, index, destination_index)
 end
 
 def reverse_array(array)
-  array.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
-  end
+  array.reverse
 end
